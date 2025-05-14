b5c2fc2c492a4fde2bd53a4ad258fb522a3561e2
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-05-14 20:28:36 +0200

    Refactor to use faster instructions

--- include/ipq/SPSCQueue.hpp
+++ include/ipq/SPSCQueue.hpp
@@ -5,7 +5,7 @@ Creator: Claudio Raimondi
 Email: claudio.raimondi@pm.me                                                   
 
 created at: 2025-05-12 18:01:10                                                 
-last edited: 2025-05-13 14:27:00                                                
+last edited: 2025-05-14 20:28:36                                                
 
 ================================================================================*/
 
@@ -28,71 +28,57 @@ class SPSCQueue : public IQueueCRTP<SPSCQueue<Item, Capacity>, Item, Capacity>
     explicit SPSCQueue(std::string_view name) : Base(name) {}
 
     template <typename ForwardItem>
-    void push_impl(ForwardItem&& item) noexcept
+    inline void push_impl(ForwardItem&& item) noexcept
     {
-      auto local_write_idx = data->write_idx.load(std::memory_order_relaxed);
-      auto next_write_idx = (local_write_idx + 1) & mask;
-
-      data->buffer[local_write_idx] = std::forward<ForwardItem>(item);
-      data->write_idx.store(next_write_idx, std::memory_order_release);
+      const auto local_write_idx = data->write_idx.fetch_add(1, /**/);
+      data->buffer[local_write_idx & mask] = std::forward<ForwardItem>(item);
     }
 
     template <typename ForwardItem>
     bool try_push_impl(ForwardItem&& item) noexcept
     {
-      auto local_write_idx = data->write_idx.load(std::memory_order_relaxed);
-      auto local_read_idx = data->read_idx.load(std::memory_order_acquire);
-      auto next_write_idx = (local_write_idx + 1) & mask;
+      const auto local_write_idx = data->write_idx.load(/**/);
+      const auto local_read_idx = data->read_idx.load(/**/);
 
-      if (next_write_idx == local_read_idx) [[unlikely]]
+      if ((local_write_idx - local_read_idx) == Capacity)
         return false;
 
-      data->buffer[local_write_idx] = std::forward<ForwardItem>(item);
-      data->write_idx.store(next_write_idx, std::memory_order_release);
-
+      data->buffer[local_write_idx & mask] = std::forward<ForwardItem>(item);
+      data->write_idx.store(local_write_idx + 1, /**/);
       return true;
     }
 
-    Item pop_impl(void) noexcept
+    inline Item pop_impl(void) noexcept
     {
-      auto local_read_idx = data->read_idx.load(std::memory_order_relaxed);
-      auto next_read_idx = (local_read_idx + 1) & mask;
-
-      Item item = std::move(data->buffer[local_read_idx]);
-      data->read_idx.store(next_read_idx, std::memory_order_release);
-
-      return item;
+      auto local_read_idx = data->read_idx.fetch_add(1, /**/);
+      return data->buffer[local_read_idx & mask];
     }
 
-    inline const Item& front_impl(void) const noexcept {
-      return data->buffer[data->read_idx.load(std::memory_order_relaxed)];
+    inline const Item& front_impl(void) const noexcept
+    {
+      auto local_read_idx = data->read_idx.load(/**/);
+      return data->buffer[local_read_idx & mask];
     }
 
-    inline bool isEmpty_impl(void) const noexcept {
-      return data->read_idx.load(std::memory_order_relaxed) == data->write_idx.load(std::memory_order_acquire);
+    inline bool isEmpty_impl(void) const noexcept
+    {
+      return data->read_idx.load(/**/) == data->write_idx.load(/**/);
     }
 
     bool isFull_impl(void) const noexcept
     {
-      auto local_write_idx = data->write_idx.load(std::memory_order_relaxed);
-      auto local_read_idx = data->read_idx.load(std::memory_order_acquire);
-      auto next_write_idx = (local_write_idx + 1) & mask;
-
-      return next_write_idx == local_read_idx;
+      return (data->write_idx.load(/**/) - data->read_idx.load(/**/) == Capacity);
     }
 
     std::size_t size_impl(void) const noexcept
     {
-      auto local_write_idx = data->write_idx.load(std::memory_order_relaxed);
-      auto local_read_idx = data->read_idx.load(std::memory_order_acquire);
-
-      return (local_write_idx - local_read_idx) & mask;
+      return data->write_idx.load(/**/) - data->read_idx.load(/**/);
     }
 
     void clear_impl(void) noexcept
     {
-      data->write_idx.store(0, std::memory_order_release);
-      data->read_idx.store(0, std::memory_order_relaxed);
+      data->write_idx.store(0, /**/);
+      data->read_idx.store(0, /**/);
     }
 };
 
